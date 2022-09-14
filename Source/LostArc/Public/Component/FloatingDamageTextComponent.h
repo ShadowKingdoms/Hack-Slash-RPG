5c10461b0fe526a59d128fc52df3f5a398a6829b
Jinho Kim <winda00zu@gmail.com>
2022-09-14 16:25:14 +0900

    1

--- Source/LostArc/Public/Component/FloatingDamageTextComponent.h
+++ Source/LostArc/Public/Component/FloatingDamageTextComponent.h
@@ -13,19 +13,37 @@ class LOSTARC_API UFloatingDamageTextComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UFloatingDamageTextComponent();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="FloatingDamageText", meta=(AllowPrivateAccess = "true"))
+	TSubclassOf<AActor> FloatingTextActorClass = nullptr;
+
+	// space between each number
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="FloatingDamageText", meta=(AllowPrivateAccess = "true"))
+	float TextVerticalSpacing = 40.0f;
+
+	// verticalOffset a new text
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="FloatingDamageText", meta=(AllowPrivateAccess = "true"))
+	float TextVerticalOffset = 100.0f;
+
+public:
+	// 월드 공간의 지정된 위치에 지정된 텍스트를 추가합니다.
+	UFUNCTION(Unreliable, BlueprintCallable, Category="FloatingDamageText")
+	void AddFloatingText(const FText& text, FVector world_location);
 
 private:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="FloatingDamage", meta=(AllowPrivateAccess = "true"))
-	TSubclassOf<AActor> FloatingTextActorClass = nullptr;
+	// callback text actor is destroyed
+	UFUNCTION()
+	void OnTextDestroyed(AActor* destroyed_actor);
+
+	// an array of all active text actors
+	UPROPERTY()
+	TArray<AActor*> ActivityTextActors;
 };
