2ab4a008dc081eb572e6cb4c847fcbf32ab262d7
ShadowKingdoms <55690757+ShadowKingdoms@users.noreply.github.com>
2023-03-19 11:06:16 +0900

    21

--- Source/LostArc/Private/AI/BTTask_TurnToTarget.cpp
+++ Source/LostArc/Private/AI/BTTask_TurnToTarget.cpp
@@ -16,15 +16,17 @@ UBTTask_TurnToTarget::UBTTask_TurnToTarget()
 EBTNodeResult::Type UBTTask_TurnToTarget::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	auto MonsterCharacter = Cast<AMonsterCharacterBase>(OwnerComp.GetAIOwner()->GetPawn());
-	if (MonsterCharacter == nullptr) return EBTNodeResult::Failed;
+	if (MonsterCharacter == nullptr)
+		return EBTNodeResult::Failed;
 
 	auto Target = Cast<ALostArcPlayerCharacter>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(AMonsterBaseAIController::TargetKey));
-	if (Target == nullptr) return EBTNodeResult::Failed;
+	if (Target == nullptr)
+		return EBTNodeResult::Failed;
 
 	FVector LookVector = Target->GetActorLocation() - MonsterCharacter->GetActorLocation();
 	LookVector.Z = 0.0f;
-	FRotator TargetRot = FRotationMatrix::MakeFromX(LookVector).Rotator();
+	const FRotator TargetRot = FRotationMatrix::MakeFromX(LookVector).Rotator();
 	MonsterCharacter->SetActorRotation(FMath::RInterpTo(MonsterCharacter->GetActorRotation(), TargetRot, GetWorld()->GetDeltaSeconds(), 5.f));
-
+	
 	return EBTNodeResult::Succeeded;
 }
